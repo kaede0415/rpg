2282943388adc3595103d01edf1a2dccff0b33c3
Glitch (special-cream-yarn) <none>
2023-02-10 15:21:39 +0000

    🏜💋 Checkpoint ./index.js:29682831/5

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ let ch_status = await channel_status.get(channel_id)
     ch_status.splice(0,1,ch_status[0]+1)
     await channel_status.set(channel_id,ch_status)
     const boss_level = ch_status[0]+1
-    if(boss_level % 50){
+    if(boss_level % 50 == 0){
       
     }
   }else{
