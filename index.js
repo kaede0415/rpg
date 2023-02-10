06b23dc8a9ba173a53df7f6458cbe375f2e82853
Glitch (special-cream-yarn) <none>
2023-02-10 14:47:47 +0000

    🐽🙍 Checkpoint ./index.js:29682831/14

--- index.js
+++ index.js
@@ -162,6 +162,8 @@ let ch_status = await channel_status.get(channel_id)
     ch_status = await channel_status.get(channel_id)
     ch_status.splice(0,1,ch_status[0]+1)
     await channel_status.set(channel_id,ch_status)
+  }else{
+    
   }
 }
 
