1aa13df6409b7afbce8051915f01ebe1c5c35148
Glitch (special-cream-yarn) <none>
2023-03-12 13:03:53 +0000

    🏙⛷ Checkpoint ./index.js:29682831/242

--- index.js
+++ index.js
@@ -125,6 +125,8 @@ async function generate_detection(player_id,message){
   if(Math.random() < probability){
     status.splice(7,1,true)
     await player_status.set(player_id,status)
+    const first_title = ["マクロ","まくろ"]
+    const second_title = ["Kenti","kenti","KENTI","検知","木僉矢口"," Detection"," detection"," DETECTION"]
     const embed = new MessageEmbed()
     .setTitle("マクロ検知")
     .setDescription("ボタンを押してください。")
